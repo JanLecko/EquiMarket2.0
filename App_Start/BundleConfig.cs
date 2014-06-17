b6d9bbf34432eac6071b35786ebd6ee49344ee26
JanLecko <janlecko@gmail.com>
2014-06-17 13:54:51 +0200

    Revert "Ajax search"
    This reverts commit dc8ee08786222dd862f5999bac82fb5bfb593df8.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -16,8 +16,7 @@ namespace EquiMarket
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
