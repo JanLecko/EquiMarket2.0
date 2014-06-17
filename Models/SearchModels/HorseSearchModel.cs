6347d1b997c42aed1811d842b58f6c9a14cd1bdc
JanLecko <janlecko@gmail.com>
2014-06-17 13:55:07 +0200

    Revert "Revert "Ajax search""
    This reverts commit b6d9bbf34432eac6071b35786ebd6ee49344ee26.

--- Models/SearchModels/HorseSearchModel.cs
+++ Models/SearchModels/HorseSearchModel.cs
@@ -7,13 +7,28 @@ namespace EquiMarket.Models.SearchModels
 {
     public class HorseSearchModel
     {
-        public int AgeFrom { get; set; }
-        public int AgeTo { get; set; }
-        public int KVHFrom { get; set; } 
-        public int KVHTo { get; set; }
+        public int? AgeFrom { get; set; }
+        public int? AgeTo { get; set; }
+        public int? KVHFrom { get; set; } 
+        public int? KVHTo { get; set; }
         public int[] Breeds { get; set; }
-        public decimal PriceFrom { get; set; }
-        public decimal PriceTo { get; set; }
+        public decimal? PriceFrom { get; set; }
+        public decimal? PriceTo { get; set; }
+
+        public bool HasValue {
+
+            get {
+                return (
+                    AgeFrom.HasValue    ||
+                    AgeTo.HasValue      ||
+                    KVHFrom.HasValue    ||
+                    KVHTo.HasValue      ||
+                    PriceFrom.HasValue  ||
+                    PriceTo.HasValue    ||
+                    (Breeds != null && Breeds.Length > 0));
+            }
+
+        }
 
     }
 }
